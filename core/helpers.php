213ce073eb59ad5343c8357aeb29314d485d743b
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-08-26 11:12:48 +0200

    Helper Slugify

--- core/helpers.php
+++ core/helpers.php
@@ -12,3 +12,28 @@ function truncate(string $text, int $length = 100): string
         return $text;
     }
 }
+
+function slugify(string $text): string
+{
+    // Strip html tags
+    $text = strip_tags($text);
+    // Replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    // Transliterate
+    setlocale(LC_ALL, 'en_US.utf8');
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // Remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    // Trim
+    $text = trim($text, '-');
+    // Remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
+    // Lowercase
+    $text = strtolower($text);
+    // Check if it is empty
+    if (empty($text)) {
+        return 'n-a';
+    }
+    // Return result
+    return $text;
+}
