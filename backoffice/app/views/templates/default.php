ba7262b90e4e33c26fa7fe22e8c058a3adcc284d
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-09-09 09:49:20 +0200

    Intégration et découpe du template backoffice

--- backoffice/app/views/templates/default.php
+++ backoffice/app/views/templates/default.php
@@ -2,13 +2,16 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include '../app/views/templates/partials/_head.php'; ?>
 </head>
 
 <body>
-    <h1>Backoffice</h1>
+    <?php include '../app/views/templates/partials/_nav.php'; ?>
+
+    <?php include '../app/views/templates/partials/_main.php'; ?>
+
+    <?php include '../app/views/templates/partials/_scripts.php'; ?>
+
 </body>
 
 </html>
